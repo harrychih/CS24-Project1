8699b8d20f39616bf01c893177f107fd21571d45
Harry Chih <yanjie@umail.ucsb.edu>
2018-10-30 10:18:33 -0700

    Add files via upload

---
+++ word.h
@@ -0,0 +1,18 @@
+#ifndef WORD_H
+#define WORD_H
+#include <iostream>
+using namespace std;
+
+class Word
+{
+public:
+	//comstructor
+	word();
+	//modifiers
+
+	//observers
+
+private:
+
+};
+#endif
